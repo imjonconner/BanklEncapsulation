42dedc4eec49657f4215fa1cae20421dc462986d
jon conner <jconnergoldleafpres@gmail.com>
2022-07-15 07:17:47 -0700

    Add project files.

---
+++ BanklEncapsulation/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanklEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+        public void Withdrawl(double amount)
+        {
+            _balance -= amount;
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine(_balance);
+        }
+           
+    }
+}
---
+++ BanklEncapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BanklEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account1 = new BankAccount();
+            Console.WriteLine("Please enter the amount you would like to deposit");
+            var amount = double.Parse(Console.ReadLine());
+            account1.Deposit(amount);
+
+            Console.WriteLine("Please enter the amount you would like to withdrawl");
+            amount = double.Parse(Console.ReadLine());
+            account1.Withdrawl(amount);
+
+            Console.WriteLine("Ok thank you");
+            Console.WriteLine("Here is your balance");
+            account1.GetBalance();
+        }
+    }
+}
